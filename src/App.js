7fb9a8ba46c4850a2ac8c2c80f6f1366bf8a1328
Hemal99 <h.githmin@gmail.com>
2022-02-11 15:00:21 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
 import { Grid } from "@mui/material";
-import LineChart from "component/Line";
+// import LineChart from "component/Line";
 import PieChart from "component/Pie";
 import BarChart from "component/Bar";
 
 function App() {
   return (
     <Grid container spacing={2}>
-      <Grid item xs={12} md={6}>
+      {/* <Grid item xs={12} md={6}>
         <LineChart />
-      </Grid>
+      </Grid> */}
 
       <Grid item xs={12} md={6}>
         <PieChart />
